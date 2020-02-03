a9852e61c8485407b372369b9a1f44e59ccced73
bolaoke <60442360+bolaoke@users.noreply.github.com>
2020-02-03 12:50:22 -0500

    Praiceweek2

---
+++ pratice.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Bolaji
+ * created :2/3/20
+ * description:Variable Pratice
+ **/
+public class pratice {
+
+	public static void main(String[] args) {
+		//Defining Variable
+		double wallet = 10.35;
+	    byte digit    = 5;
+	    int bag      = 3;
+	    
+	    //Declaring Variables
+	    
+	    String name1;
+	   String name2;
+	   String name3;
+	    
+	    //Assigning values
+	   
+	     name1 = "John";
+	     name2 = "Pat";
+	     name3 = "Ken";
+	     
+	     //Printing
+	     System.out.println(name1);
+	     System.out.println(name1+name3);
+		// TODO Auto-generated method stub
+
+	}
+
+}
