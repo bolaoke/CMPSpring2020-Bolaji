43e265b9da0653f614d0ad2bbf00f75326622ee7
bolaoke <60442360+bolaoke@users.noreply.github.com>
2020-02-05 16:02:43 -0500

    praticeweek4a

---
+++ src/Praticeweek4a.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author bolaji
+ * created:2/5/20
+ * description:
+ *
+ **/
+import java.util.Scanner;
+public class Praticeweek4a {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		//ask user for input
+		System.out.println("Enter a number:");
+		
+		//defining Variable
+		int num1 = keyboard.nextInt();
+       int num2 = keyboard.nextInt();
+       int num3 = keyboard.nextInt();
+       
+       //Printing
+      System.out.println  (num1*num2*num3);
+		
+	}
+
+}
