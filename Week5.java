8c77b25118e06ab17faa74fca062269fb515f42f
bolaoke <60442360+bolaoke@users.noreply.github.com>
2020-03-01 22:19:33 -0500

    Add files via upload

---
+++ Week5.java
@@ -0,0 +1,25 @@
+  import java.util.Scanner;
+  
+  public class Week5 {
+  public static void main(String[] args) {
+		Scanner scnr = new Scanner (System.in);
+		
+		int start = 5;
+		
+		
+		
+		System.out.println(" Enter a number: ");
+		 start  = scnr. nextInt();
+		 
+		 for(int i = 5; i < 99; i+=3) {
+			 System.out.println(i);
+			 
+		 }
+		
+		
+		
+		
+
+	}
+
+}
