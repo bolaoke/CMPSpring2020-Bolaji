72efa929542b8b257d87d829b6f924707a2636b3
bolaoke <60442360+bolaoke@users.noreply.github.com>
2020-03-04 01:22:55 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @author bolaji
+ *
+ **/
+public class Main {
+
+	public static void Week5c(String[] args) {
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 10; i++);
+		 sum +=i;
+		 
+		 System.out.print(sum);
+		 
+		 
+		  int sum = 0;
+		  
+		 double average = 0.0;
+		 for(int i = 1; i <=10;i++);
+		   average = sum /10;
+		   System.out.println(average);
+			
+		 
+		   
+		   int power = 2;
+		   
+		   for (int i = 0; i < 2; i++);
+		    power = power * 2;
+		    
+		    System.out.print(power);
+		    
+		    
+		    
+		    
+		    
+		    int multi = 1;
+		    int factorial = 6;
+		    for (int i = 1; i < multi;i++);
+		    multi * = i;
+		     System.out.println(multi);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
